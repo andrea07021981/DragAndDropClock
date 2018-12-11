f37a4d3dcc641c13307dbfddf7af60622ed44d74
Andrea Franco <andrea.franco@overit.it>
2018-12-11 19:12:15 +0100

    Initial commit

---
+++ app/src/main/java/com/projects/andreafranco/draganddropclock/MainActivity.java
@@ -0,0 +1,194 @@
+package com.projects.andreafranco.draganddropclock;
+
+import android.content.ClipData;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int INTERVAL = 1000; //1 millisecond
+    private static final int TIME_LOADER_ID = 1;
+
+    LinearLayout mTopLayout;
+    LinearLayout mBottomLayout;
+    TextView mDraggableImageview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDraggableImageview = findViewById(R.id.draggable_image_view);
+        mDraggableImageview.setOnTouchListener(new MyTouchListener());
+        mTopLayout = findViewById(R.id.top_layout);
+        mTopLayout.setOnDragListener(new MyDragListener());
+        mBottomLayout = findViewById(R.id.bottom_layout);
+        mBottomLayout.setOnDragListener(new MyDragListener());
+        callAsynchronousTask();
+    }
+
+    public void callAsynchronousTask() {
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+        TimerTask doAsynchronousTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                            AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+
+                                @Override
+                                protected Void doInBackground(Void... voids) {
+                                    RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                                    String url ="http://dateandtimeasjson.appspot.com/";
+
+                                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                                            new Response.Listener<String>() {
+                                                @Override
+                                                public void onResponse(String response) {
+                                                    // Display the first 500 characters of the response string.
+                                                    mDraggableImageview.setText("Response is: "+ parseJsonResponse(response));
+                                                }
+
+                                                private String parseJsonResponse(String response) {
+                                                    JSONObject root = null;
+                                                    try {
+                                                        root = new JSONObject(response.replace("datetime(", "").replace(")", ""));
+                                                        String dateTime = root.getString("datetime");
+
+                                                       return dateTime;
+                                                    } catch (JSONException e) {
+                                                        e.printStackTrace();
+                                                    }
+                                                    return null;
+                                                }
+                                            }, new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+                                            mDraggableImageview.setText("That didn't work!");
+                                        }
+                                    });
+
+                                    queue.add(stringRequest);
+                                    return null;
+                                }
+                            };
+                            task.execute();
+                        } catch (Exception e) {
+
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(doAsynchronousTask, 1000, 1000); //execute in every 50000 ms
+    }
+
+    private final class MyTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+                        view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    class MyDragListener implements View.OnDragListener {
+        Drawable enterShape = getResources().getDrawable(
+                R.drawable.shape_droptarget);
+        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    slideUp(mBottomLayout);
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    v.setBackgroundDrawable(enterShape);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    v.setBackgroundDrawable(normalShape);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign View to ViewGroup
+                    View view = (View) event.getLocalState();
+                    ViewGroup owner = (ViewGroup) view.getParent();
+                    owner.removeView(view);
+                    LinearLayout container = (LinearLayout) v;
+                    container.addView(view);
+                    view.setVisibility(View.VISIBLE);
+                    if (container.getId() != R.id.bottom_layout) slideDown(mBottomLayout);
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    v.setBackgroundDrawable(normalShape);
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+
+    // slide the view from below itself to the current position
+    public void slideUp(View view){
+        //view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                view.getHeight(),  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    // slide the view from its current position to below itself
+    public void slideDown(View view){
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,                 // fromYDelta
+                view.getHeight() / 2); // toYDelta
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+}
